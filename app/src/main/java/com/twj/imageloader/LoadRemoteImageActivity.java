b5eeb9c8c148d428d4d59652269207c74a408293
tangwenjing <hust_twj@163.com>
2019-05-22 10:16:33 +0800

    [feature]增加加载本地和网络图片页面

---
+++ app/src/main/java/com/twj/imageloader/LoadRemoteImageActivity.java
@@ -0,0 +1,18 @@
+package com.twj.imageloader;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * description ：加载网络图片
+ * Created by Wenjing.Tang on 2019-05-22.
+ */
+public class LoadRemoteImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_remote_image);
+    }
+}
--- app/src/main/java/com/twj/imageloader/MainActivity.kt
+++ app/src/main/java/com/twj/imageloader/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.twj.imageloader
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_load_local.setOnClickListener {
+            startActivity(Intent(this@MainActivity, LoadLocalImageActivity::class.java))
+        }
+
+        tv_load_remote.setOnClickListener {
+            startActivity(Intent(this@MainActivity, LoadRemoteImageActivity::class.java))
+        }
+
     }
 }
