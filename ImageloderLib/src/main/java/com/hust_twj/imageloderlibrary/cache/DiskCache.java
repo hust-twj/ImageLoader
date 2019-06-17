541cda327fe0cba41897af3ab331ed0c2cc7537a
hust_twj <254062403@qq.com>
2019-06-18 00:43:43 +0800

    [feature]完善内存缓存

--- ImageloderLib/src/main/java/com/hust_twj/imageloderlibrary/cache/DiskCache.java
+++ ImageloderLib/src/main/java/com/hust_twj/imageloderlibrary/cache/DiskCache.java
@@ -1,12 +1,15 @@
 package com.hust_twj.imageloderlibrary.cache;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 
 import com.hust_twj.imageloderlibrary.utils.Md5Utils;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,15 +17,75 @@ import java.io.OutputStream;
 /**
  * Description ：磁盘缓存
  * Created by Wenjing.Tang on 2019-06-16.
- *
+ * <p>
  * 参考：https://www.jianshu.com/p/f9cfbea586c2
  */
 public class DiskCache implements BitmapCache {
 
     private DiskLruCache mDiskLruCache;
 
-    public DiskCache(Context context) {
+    private static final String IMAGE_DISK_CACHE = "bitmap";
+    /**
+     * 缓存最大值
+     */
+    private static final int MAX_SIZE = 20 * 1024 * 1024;
 
+    private static DiskCache mInstance;
+
+    public DiskCache(Context context) {
+        initDiskCache(context);
+    }
+
+    public static DiskCache getInstance(Context context) {
+        if (mInstance == null) {
+            synchronized (DiskCache.class) {
+                if (mInstance == null) {
+                    mInstance = new DiskCache(context);
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    private void initDiskCache(Context context) {
+        try {
+            File cacheDir = getDiskCacheDir(context, IMAGE_DISK_CACHE);
+            if (!cacheDir.exists()) {
+                cacheDir.mkdirs();
+            }
+            mDiskLruCache = DiskLruCache.open(cacheDir, getAppVersion(context), 1, MAX_SIZE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int getAppVersion(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    /**
+     * 获取内存缓存路径
+     * external：如：/storage/emulated/0/Android/data/package_name/cache
+     * internal 如：/data/data/package_name/cache
+     *
+     * @param context    context
+     * @param uniqueName 缓存后缀
+     */
+    public static File getDiskCacheDir(Context context, String uniqueName) {
+        String cachePath;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                && context.getExternalCacheDir() != null) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return new File(cachePath + File.separator + uniqueName);
     }
 
     @Override
@@ -41,7 +104,6 @@ public class DiskCache implements BitmapCache {
         return null;
     }
 
-
     /**
      * 内存缓存只缓存从网络下载的图片
      *
@@ -50,7 +112,7 @@ public class DiskCache implements BitmapCache {
      */
     @Override
     public void put(String key, Bitmap value) {
-        DiskLruCache.Editor editor = null;
+        DiskLruCache.Editor editor;
         try {
             // 如果没有找到对应的缓存，则准备从网络上请求数据，并写入缓存
             editor = mDiskLruCache.edit(Md5Utils.toMD5(key));
