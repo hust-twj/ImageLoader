7036599d67e540037e7fa5694ed116b1227ddc34
hust_twj <254062403@qq.com>
2019-08-03 16:47:28 +0800

    [feature]删除无用代码

--- ImageloaderLib/src/main/java/com/hust_twj/imageloderlibrary/utils/IOUtil.java
+++ ImageloaderLib/src/main/java/com/hust_twj/imageloderlibrary/utils/IOUtil.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2010 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.hust_twj.imageloderlibrary.utils;
 
 import java.io.Closeable;
@@ -23,16 +7,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.StringWriter;
-import java.nio.charset.Charset;
 
 /**
- * Junk drawer of utility methods.
+ * IO工具类
  */
 public final class IOUtil {
-
-
-    private IOUtil() {
-    }
 
     public static String readFully(Reader reader) throws IOException {
         try {
@@ -49,8 +28,7 @@ public final class IOUtil {
     }
 
     /**
-     * Deletes the contents of {@code dir}. Throws an IOException if any file
-     * could not be deleted, or if {@code dir} is not a readable directory.
+     * 删除对应路径的内容
      */
     public static void deleteContents(File dir) throws IOException {
         File[] files = dir.listFiles();
@@ -67,7 +45,7 @@ public final class IOUtil {
         }
     }
 
-    public static void closeQuietly(/* Auto */Closeable closeable) {
+    public static void closeQuietly(Closeable closeable) {
         if (closeable != null) {
             try {
                 closeable.close();
@@ -85,7 +63,6 @@ public final class IOUtil {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public static void close(OutputStream out) {
